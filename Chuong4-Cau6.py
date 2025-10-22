4fec6f7cd1564b69e153b94692a8a0e8d9113529
Khoa245441 <khoa_dpm245441@studen.agu.edu.vn>
2025-10-22 23:28:14 +0700

    https://github.com/khoadpm245441/DPM245441_DH25PM_LENGUYENDANGKHOA_NHOM4_TO1_NOPBAITAP_PYTHON

--- Chuong4-Cau6.py
+++ Chuong4-Cau6.py
@@ -1,2 +1,5 @@
-from random import randrange
-randrange(0, 100)
+import random
+
+# Tạo một số ngẫu nhiên trong khoảng từ 0 đến 99
+n = random.randrange(0, 100)
+print("Giá trị ngẫu nhiên từ randrange(0,100):", n)
